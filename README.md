200fdd74e4cab3c68d9e7d6fa4bc791893e20fd1
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-06-13 16:10:57 -0500

    basic file structure

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # fictional-character-assignment
+
+## To Do
+
+[x]npm init, dependencies, file structure
+[]basic html - header footer etc.
+[]learn bootstrap
+[]create three partials for each fictional character
---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+app.use(express.static('/public'));
+app.listen(process.env.PORT || 7575);
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve('public/views/index.html'));
+});
